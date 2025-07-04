9be67e6d3d702434038ba415799335cd878540da
MonkeyPlay <andreas.vrhovsek@gmail.com>
2025-07-04 23:36:43 +0100

    Create TimescaleDB.sql

---
+++ TimescaleDB.sql
@@ -0,0 +1,11 @@
+CREATE TABLE raw_content (
+    source TEXT,
+    author TEXT,
+    verified BOOLEAN,
+    url TEXT,
+    title TEXT,
+    body TEXT,
+    engagement INTEGER,
+    publisher TEXT,
+    timestamp TIMESTAMPTZ
+);
