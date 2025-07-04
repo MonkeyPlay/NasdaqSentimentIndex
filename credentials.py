0063c477a2d969d8fc5114fc84e58ceebacf9024
MonkeyPlay <andreas.vrhovsek@gmail.com>
2025-07-04 23:40:09 +0100

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,8 @@
+TWITTER_API_KEY = "your_api_key"
+TWITTER_API_SECRET = "your_api_secret"
+KEYWORDS = "Nasdaq OR AAPL OR Fed"  # Or: $NQ, $QQQ etc.
+SINCE_TIME = "2025-04-30"
+NEWS_URLS = [
+    "https://www.bloomberg.com/news/articles/2025-04-30/...",
+    "https://www.cnbc.com/2025/04/30/..."
+]
